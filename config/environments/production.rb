969182283bb6d25b38f9c90ac06eff07e7faa69c
Brian Kelsey <brian.kelsey2@gmail.com>
2012-07-16 13:43:23 -0400

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,10 @@ SampleApp::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
+  # Force all access to the app over SSL, use Strict-Transport-Security, 
+  # and use secure cookies.
+  config.force_ssl = true
+
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
