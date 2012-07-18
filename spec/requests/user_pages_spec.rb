90e76019c3c3e77cf454fb9efc28a6182dcb9a6c
Brian Kelsey <brian.kelsey2@gmail.com>
2012-07-18 16:03:02 -0400

    finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -35,7 +35,11 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+
+        page.should have_link('Sign out')
       end
+
+
     end
   end
 
